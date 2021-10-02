9456170ad0fc21511264429ffd71a69ade8dd907
Hudson Thomas <hudsonthomas2006@gmail.com>
2021-10-02 09:18:53 -0400

    updated urls.py

--- project/threads/urls.py
+++ project/threads/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .api import (create_civi, delete_civi, edit_civi, edit_thread, get_civi,
@@ -18,24 +18,24 @@ router.register(r"civis", CiviViewSet)
 router.register(r"accounts", ProfileViewSet)
 
 urlpatterns = [
-    url(r"^v1/", include(router.urls)),
+    path(r"^v1/", include(router.urls)),
 ]
 
 urlpatterns += [
-    url(r"^thread_data/(?P<thread_id>\w+)/$", get_thread, name="get thread"),
-    url(r"^civi_data/(?P<civi_id>\w+)$", get_civi, name="get civi"),
-    url(r"^threads/(?P<thread_id>\w+)/civis$", get_civis, name="get civis"),
-    url(
+    path(r"^thread_data/(?P<thread_id>\w+)/$", get_thread, name="get thread"),
+    path(r"^civi_data/(?P<civi_id>\w+)$", get_civi, name="get civi"),
+    path(r"^threads/(?P<thread_id>\w+)/civis$", get_civis, name="get civis"),
+    path(
         r"^response_data/(?P<thread_id>\w+)/(?P<civi_id>\w+)/$",
         get_responses,
         name="get responses",
     ),
-    url(r"^new_thread/$", new_thread, name="new thread"),
-    url(r"^edit_thread/$", edit_thread, name="edit thread"),
-    url(r"^new_civi/$", create_civi, name="new civi"),
-    url(r"^rate_civi/$", rate_civi, name="rate civi"),
-    url(r"^edit_civi/$", edit_civi, name="edit civi"),
-    url(r"^delete_civi/$", delete_civi, name="delete civi"),
-    url(r"^upload_images/$", upload_civi_image, name="upload images"),
-    url(r"^upload_image/$", upload_thread_image, name="upload image"),
+    path(r"^new_thread/$", new_thread, name="new thread"),
+    path(r"^edit_thread/$", edit_thread, name="edit thread"),
+    path(r"^new_civi/$", create_civi, name="new civi"),
+    path(r"^rate_civi/$", rate_civi, name="rate civi"),
+    path(r"^edit_civi/$", edit_civi, name="edit civi"),
+    path(r"^delete_civi/$", delete_civi, name="delete civi"),
+    path(r"^upload_images/$", upload_civi_image, name="upload images"),
+    path(r"^upload_image/$", upload_thread_image, name="upload image"),
 ]
